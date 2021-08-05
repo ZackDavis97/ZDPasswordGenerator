72aafe23e01d6a7060855a12d839fc9cb0972dcc
Zack Davis <jantrezac95@gmail.com>
2021-08-05 14:51:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+For this homework assignment I was tasked with creating a random password generator. This assignment proved to be challenging because of remembering the command functions and going back and scrutinizing previous code that seemed correct because I misunderstood the function. In the end I was able to successfully create a generator that could spit out passwords between 10-25 characters long.
+## Highlights of the assignment
+* Line 30-65 was simple in the aspect it was a copy/paste and change the variables to what I needed them to be.
+* Each specially defined character was set up for a loop in array.
+* I originally had the characters in one continuous string from 11-14, but sparced them out into their own.
+* The only alert someone will receive if is if they want a password shorter/greater than the requirements in the prompt.
+* A math floor was created so I would be able to get whole numbers.
+* The string statements in the js do have some slight variation in the worind to give the experience some character.
+
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var generateBtn = document.querySelector("#generate");
 
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -17,7 +16,7 @@ var numeric = ["0","1","2","3","4","5","6","7","8","9"];
 var constant = [];
 
 function generatePassword(){
-
+  
   var passwordLength = prompt("How many characters do you want your password to be 10-25")
   console.log(passwordLength)
   var passwordLength = parseInt(passwordLength);
@@ -29,63 +28,65 @@ function generatePassword(){
   }
   
   var capsConfirm = confirm("Do you want any uppercase letters in your passphrase?")
-    if (capsConfirm === true) {
-      //this looks like we are going to repeat this loop for as many characters in the caps array
-      for (let i = 0; i < caps.length; i++) {
-        constant.push(caps[i]);
-        // console.log("this is the only value in constant",constant)
-      }
+  if (capsConfirm === true) {
+    //this looks like we are going to repeat this loop for as many characters in the caps array
+    for (let i = 0; i < caps.length; i++) {
+      constant.push(caps[i]);
+      // console.log("this is the only value in constant",constant)
     }
+  }
   
-    var lowerConfirm = confirm("Do you want any lowercase letters in your passphrase?")
-    if (lowerConfirm === true) {
-      for (let i = 0; i < lower.length; i++) {
-        constant.push(lower[i]);
-      }
+  var lowerConfirm = confirm("Do you want any lowercase letters in your passphrase?")
+  if (lowerConfirm === true) {
+    for (let i = 0; i < lower.length; i++) {
+      constant.push(lower[i]);
     }
-    console.log("constant",constant)
-
-
-    var specialConfirm = confirm("Do you want any special characters in thy password?")
-    if (specialConfirm === true) {
-      for (let i = 0; i < special.length; i++) {
-        constant.push(special[i]);
-        
-      }
+  }
+  console.log("constant",constant)
+  
+  
+  var specialConfirm = confirm("Do you want any special characters in thy password?")
+  if (specialConfirm === true) {
+    for (let i = 0; i < special.length; i++) {
+      constant.push(special[i]);
+      
     }
-    console.log("constant",constant)
-
-
-    var numericConfirm = confirm("Do you want any numbers in thy password?")
-    if (numericConfirm === true) {
-      for (let i = 0; i < numeric.length; i++) {
-        constant.push(numeric[i]);
-        
-      }
+  }
+  console.log("constant",constant)
+  
+  
+  var numericConfirm = confirm("Do you want any numbers in thy password?")
+  if (numericConfirm === true) {
+    for (let i = 0; i < numeric.length; i++) {
+      constant.push(numeric[i]);
+      
     }
-    console.log("constant",constant)
-
-    var randomPassword = "";
-    console.log(passwordLength)
-    for (let i = 0; i < passwordLength; i++) {
-      //passwordlength is not a array so we cant index it
-      //our goal is to loop and grab numbers from the constant array not the password length
-      var randomNumber = Math.floor(Math.random() * constant.length);
-      console.log(randomNumber)
-      console.log(constant[randomNumber])
-        randomPassword 
-          constant [
-            Math.floor(Math.random() * constant.length)];
- 
+  }
+  console.log("constant",constant)
+  
+  var randomPassword = "";
+  console.log(passwordLength)
+  for (let i = 0; i < passwordLength; i++) {
+    //passwordlength is not a array so we cant index it
+    //our goal is to loop and grab numbers from the constant array not the password length
+    var randomNumber = Math.floor(Math.random() * constant.length);
+    console.log(randomNumber)
+    console.log(constant[randomNumber])
+    randomPassword +=
+    constant [
+      Math.floor(Math.random() * constant.length)];
+      
     }
-
-return "password";
-
-}
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
+    //if I want the password to generate I need to make sure the return matches the var
+    return randomPassword;
+    
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
+  
+  
+  
+  
+  
